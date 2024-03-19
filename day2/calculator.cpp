f23e1f74e29523c5cba3c8441458b5c32c41498f
Rip_CuteBacon_Is_God <minthway2008@gmail.com>
2024-03-19 19:10:21 +0630

    calculator

---
+++ day2/calculator.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x,y;
+    cout << "This is Sample C++ Calculator. \n\n";
+    cout << "Please Enter First Number: ";
+    cin >> x;
+    cout << "Please Enter Second Number: ";
+    cin >>y;
+    cout << "Your answer is: " << x+y;
+    return 0;
+}
